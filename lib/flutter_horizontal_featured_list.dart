4bf57bf3e65f352c6b38b35c9daaf0a08f13de58
Hung Luong Do Minh <hung.luongdm@homecredit.vn>
2021-06-11 15:07:48 +0700

    change total height to list view item height

--- lib/flutter_horizontal_featured_list.dart
+++ lib/flutter_horizontal_featured_list.dart
@@ -55,8 +55,8 @@ import 'package:flutter/material.dart';
 /// ```
 /// {@end-tool}
 class HorizontalFeaturedList extends StatefulWidget {
-  /// is total height of this widget.
-  final double height;
+  /// lets the [ListView] knows its children size, caused by it is placed inside a [Column].
+  final double itemHeight;
 
   /// is the [TextStyle] use for <Title>.
   final TextStyle titleTextStyle;
@@ -110,15 +110,14 @@ class HorizontalFeaturedList extends StatefulWidget {
   /// - <Featured List View> is created by [itemBuilder], [itemCount] at the bottom.
   ///
   /// This widget has a default design helps you in saving time to create many of same designs.
-  /// - It need a [height], this is total height of this widget, lets the [Column] inside knows its children size.
   /// - [margin] : this is the spacing from parent to the left side of <Title>, and from parent to the right side of <See All>, and spacing between <Featured List View> items also.
-  /// - Each item in <Featured List View> has a default style with [itemPadding], [overlayColor] is used for pressing animation, [itemBorderRadius] is the border radius, [onPressedItem] to navigate to a detail screen, [itemWidth] lets the horizontal [ListView] knows its children size.
+  /// - Each item in <Featured List View> has a default style with [itemPadding], [overlayColor] is used for pressing animation, [itemBorderRadius] is the border radius, [onPressedItem] to navigate to a detail screen, [itemWidth] & [itemHeight] lets the horizontal [ListView] knows its children size.
   /// - [itemColorBuilder] : is background color of an item.
   /// - [animationDuration] : to run the default animation :D, use [Duration.zero] if you don't want it.
 
   const HorizontalFeaturedList({
     Key? key,
-    this.height = 165,
+    this.itemHeight = 120,
     this.titleTextStyle = const TextStyle(
       fontSize: 18,
       fontWeight: FontWeight.bold,
@@ -170,102 +169,100 @@ class _HorizontalFeaturedListState extends State<HorizontalFeaturedList>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: widget.height,
-      child: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-                left: widget.margin, right: max(0.0, widget.margin - 12)),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    widget.titleText,
-                    style: widget.titleTextStyle,
-                  ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(
+              left: widget.margin, right: max(0.0, widget.margin - 12)),
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  widget.titleText,
+                  style: widget.titleTextStyle,
                 ),
-                ElevatedButton(
-                  child: Text(
-                    widget.seeAllText,
-                    style: widget.seeAllTextStyle,
-                  ),
-                  onPressed: widget.onPressedSeeAll,
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<OutlinedBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8))),
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                        EdgeInsets.all(12)),
-                    elevation: MaterialStateProperty.all<double>(0.0),
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.transparent),
-                    overlayColor:
-                        MaterialStateProperty.all<Color>(widget.overlayColor),
-                    minimumSize: MaterialStateProperty.all<Size>(Size.zero),
-                  ),
+              ),
+              ElevatedButton(
+                child: Text(
+                  widget.seeAllText,
+                  style: widget.seeAllTextStyle,
                 ),
-              ],
-            ),
+                onPressed: widget.onPressedSeeAll,
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8))),
+                  padding:
+                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(12)),
+                  elevation: MaterialStateProperty.all<double>(0.0),
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.transparent),
+                  overlayColor:
+                      MaterialStateProperty.all<Color>(widget.overlayColor),
+                  minimumSize: MaterialStateProperty.all<Size>(Size.zero),
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 4),
-          Expanded(
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: widget.itemCount,
-                itemBuilder: (context, index) {
-                  final itemWidth = widget.itemWidth ??
-                      MediaQuery.of(context).size.width * 0.75;
-                  final begin = Offset(-index * 0.95, 0);
-                  final firstWeight = 0.5;
-                  return SlideTransition(
-                    position: TweenSequence<Offset>([
-                      TweenSequenceItem<Offset>(
-                          tween: Tween<Offset>(
-                            begin: begin,
-                            end: begin,
-                          ),
-                          weight: firstWeight),
-                      TweenSequenceItem<Offset>(
-                          tween: Tween<Offset>(
-                            begin: begin,
-                            end: Offset.zero,
-                          ).chain(CurveTween(curve: Curves.linearToEaseOut)),
-                          weight: 1 - firstWeight),
-                    ]).animate(_animationController),
-                    child: Container(
-                      width: itemWidth,
-                      margin: EdgeInsets.only(
-                          left: index == 0 ? widget.margin : 0,
-                          right: widget.margin),
-                      child: ElevatedButton(
-                        child: widget.itemBuilder(context, index),
-                        onPressed: widget.onPressedItem,
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all<OutlinedBorder>(
-                              RoundedRectangleBorder(
-                                  borderRadius: widget.itemBorderRadius)),
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                              widget.itemPadding),
-                          elevation: MaterialStateProperty.all<double>(0.0),
-                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              widget.itemColorBuilder != null
-                                  ? widget.itemColorBuilder!(context, index)
-                                  : Colors.transparent),
-                          overlayColor: MaterialStateProperty.all<Color>(
-                              widget.overlayColor),
-                          minimumSize:
-                              MaterialStateProperty.all<Size>(Size.zero),
+        ),
+        const SizedBox(height: 4),
+        Container(
+          height: widget.itemHeight,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.itemCount,
+              itemBuilder: (context, index) {
+                final itemWidth = widget.itemWidth ??
+                    MediaQuery.of(context).size.width * 0.75;
+                final begin = Offset(-index * 0.95, 0);
+                final firstWeight = 0.5;
+                return SlideTransition(
+                  position: TweenSequence<Offset>([
+                    TweenSequenceItem<Offset>(
+                        tween: Tween<Offset>(
+                          begin: begin,
+                          end: begin,
                         ),
+                        weight: firstWeight),
+                    TweenSequenceItem<Offset>(
+                        tween: Tween<Offset>(
+                          begin: begin,
+                          end: Offset.zero,
+                        ).chain(CurveTween(curve: Curves.linearToEaseOut)),
+                        weight: 1 - firstWeight),
+                  ]).animate(_animationController),
+                  child: Container(
+                    width: itemWidth,
+                    margin: EdgeInsets.only(
+                        left: index == 0 ? widget.margin : 0,
+                        right: widget.margin),
+                    child: ElevatedButton(
+                      child: widget.itemBuilder(context, index),
+                      onPressed: widget.onPressedItem,
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                            RoundedRectangleBorder(
+                                borderRadius: widget.itemBorderRadius)),
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                            widget.itemPadding),
+                        elevation: MaterialStateProperty.all<double>(0.0),
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            widget.itemColorBuilder != null
+                                ? widget.itemColorBuilder!(context, index)
+                                : Colors.transparent),
+                        overlayColor: MaterialStateProperty.all<Color>(
+                            widget.overlayColor),
+                        minimumSize: MaterialStateProperty.all<Size>(Size.zero),
                       ),
                     ),
-                  );
-                }),
-          ),
-        ],
-      ),
+                  ),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
